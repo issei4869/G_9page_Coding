1058ab8ca42646dcf1096a2e1aef188eb415ee2d
長谷川一輝 <e3nr3hwtl.87269.yu@gmail.com>
2024-09-22 02:28:45 +0900

    ヘッダー作成途中

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,36 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+    /////////////////////// ドロワーメニュー ///////////////////////
+    $('.js-hamburger').click(function () {
+        if ($('.js-hamburger').hasClass('is-active')) {
+        // メニューが閉じられるときの処理
+        $('.js-hamburger').removeClass('is-active');
+        $('.js-sp-nav').fadeOut(300);
+        $('body').removeClass('is-scroll'); // body要素のスクロール固定をOFF
+        } else {
+        // メニューが開かれるときの処理
+        $('.js-hamburger').addClass('is-active');
+        $('.js-sp-nav').fadeIn(300);
+        $('body').addClass('is-scroll'); // body要素のスクロール固定をON
+        }
+    });
 
+    // ページ内リンクをクリックするとメニューが閉じる
+    $('.js-sp-nav a[href]').on('click', function (event) {
+        $('.js-hamburger').removeClass('is-active');
+        $('.js-sp-nav').fadeOut(300);
+        $('body').removeClass('is-scroll'); // body要素のスクロール固定をOFF
+    });
+
+    // 画面幅が768px以上になるとメニューが閉じる
+    $(window).resize(function () {
+        if ($(window).width() >= 768) {
+        if ($('.js-hamburger').hasClass('is-active')) {
+            $('.js-hamburger').removeClass('is-active');
+            $('.js-sp-nav').fadeOut(300);
+            $('body').removeClass('is-scroll'); // body要素のスクロール固定をOFF
+        }
+        }
+    });
 
 });
